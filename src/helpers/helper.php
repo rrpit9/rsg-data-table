ef3994dbf423ae9dbc06a9f1ebf0d0fe6d035a09
Rajeev Ranjan <56844776+rrpit9@users.noreply.github.com>
2022-12-19 10:52:40 +0530

    Update helper.php

--- src/helpers/helper.php
+++ src/helpers/helper.php
@@ -1,16 +1,16 @@
 <?php
-if (! function_exists('studly_case')) {
-    /*Convert a value to studly caps case.*/
-    function studly_case($value)
-    {
-        return Illuminate\Support\Str::studly($value);
+    if (! function_exists('studly_case')) {
+        /*Convert a value to studly caps case.*/
+        function studly_case($value)
+        {
+            return Illuminate\Support\Str::studly($value);
+        }
     }
-}
 
-if (! function_exists('str_singular')) {
-    /*Get the singular form of an English word.*/
-    function str_singular($value)
-    {
-        return Illuminate\Support\Str::singular($value);
+    if (! function_exists('str_singular')) {
+        /*Get the singular form of an English word.*/
+        function str_singular($value)
+        {
+            return Illuminate\Support\Str::singular($value);
+        }
     }
-}
